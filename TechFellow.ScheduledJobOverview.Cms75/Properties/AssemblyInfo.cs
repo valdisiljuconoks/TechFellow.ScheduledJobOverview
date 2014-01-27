c58dbeb52bbcb2e9b14551b7e257cbb16df6d487
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2014-01-28 00:41:42 +0200

    Added support for EPiServer 7.5

---
+++ TechFellow.ScheduledJobOverview.Cms75/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Web.UI;
+using TechFellow.ScheduledJobOverview;
+
+[assembly: AssemblyTitle("TechFellow.ScheduledJobOverview.Cms75")]
+[assembly: AssemblyDescription("EPiServer Scheduled job overview plugin. Gives you an easy way to overview all of your scheduled jobs.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tech Fellow Consulting")]
+[assembly: AssemblyProduct("EPiServer 7.5 Scheduled Job Overview")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("70780A25-E73F-4C98-B611-AD6E15E448FE")]
+[assembly: InternalsVisibleTo("ScheduledJobOverview.UnitTests")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: WebResource(Const.ModuleName + ".Scripts.angular.min.js", "text/javascript")]
+[assembly: WebResource(Const.ModuleName + ".Scripts.site.js", "text/javascript")]
+[assembly: WebResource(Const.ModuleName + ".Scripts.site.min.js", "text/javascript")]
+[assembly: WebResource(Const.ModuleName + ".Images.spinner.gif", "img/gif")]
