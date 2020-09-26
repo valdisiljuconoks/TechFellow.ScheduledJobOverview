26d9e24c63f70d194a14c97d7b7edb55f786374f
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2020-09-26 16:12:09 +0300

    Version update

--- src/ScheduledJobOverview/Properties/AssemblyInfo.cs
+++ src/ScheduledJobOverview/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -7,12 +7,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tech Fellow Consulting, https://tech-fellow.net")]
 [assembly: AssemblyProduct("EPiServer Scheduled Job Overview")]
-[assembly: AssemblyCopyright("Copyright 2017 (c) Valdis Iljuconoks, Tech Fellow")]
+[assembly: AssemblyCopyright("Copyright 2020 (c) Valdis Iljuconoks, Tech Fellow")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("14686023-2898-4b8b-a503-d802c1429f73")]
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
-[assembly: AssemblyInformationalVersion("4.0.0")]
+[assembly: AssemblyVersion("4.1.0.0")]
+[assembly: AssemblyFileVersion("4.1.0.0")]
+[assembly: AssemblyInformationalVersion("4.1.0")]
 [assembly: InternalsVisibleTo("ScheduledJobOverview.UnitTests")]
